1e20b8c92f4bd263d62690807f5f775a5274e45c
rinwindows <phurinmeng@gmail.com>
2023-11-07 22:48:50 -0800

    fixed overflow need help from ta tho

--- lab5/excercise1.s
+++ lab5/excercise1.s
@@ -22,6 +22,7 @@ main:
     li $v0, 5
     syscall
     add $t0, $0, $v0    #s0 contains multiplicand
+    add $s0, $t0, $0    # save for overflow calculation
 
     li $v0, 4
     la $a0, secondnum
@@ -30,13 +31,23 @@ main:
     li $v0, 5
     syscall
     add $t1, $0, $v0    #s1 contains multiplier
+    add $s1, $t1, $0    # save for overflow calculation
 
     add $t2, $0, $0     # product
 
 multiply:
+
     andi $t3, $t1, 1    # check if current bit is a 1
     beqz $t3, isZero
+    # first save $t0 and $t2 to check for overflow
+
     addu $t2, $t2, $t0
+
+    # now calculate if there is overflow
+    xor $s2, $s0, $t2   # $s2 contains the sign bit of $s0 and $t2
+    xor $s3, $s1, $t2   # $s3 contains the sign bit of $s1 and $t2
+    and $s4, $s2, $s3   # $s4 contains the sign bit of $s0 and $s1
+    bltz $s4, error     # if $s4 is negative, then there is overflow
     
 
 isZero:
